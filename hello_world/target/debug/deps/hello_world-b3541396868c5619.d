c8e1e7229b1c84bb92d2f1e3942e02c2570dd76b
mansiverma897993 <vmansi756@gmail.com>
2025-10-21 14:43:57 +0000

    Added hello world

---
+++ hello_world/target/debug/deps/hello_world-b3541396868c5619.d
@@ -0,0 +1,5 @@
+/home/mansi_verma/TurbinePB_Q425_mansiverma897993/hello_world/target/debug/deps/hello_world-b3541396868c5619.d: src/main.rs
+
+/home/mansi_verma/TurbinePB_Q425_mansiverma897993/hello_world/target/debug/deps/hello_world-b3541396868c5619: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/mansi_verma/TurbinePB_Q425_mansiverma897993/hello_world/target/debug/hello_world: /home/mansi_verma/TurbinePB_Q425_mansiverma897993/hello_world/src/main.rs
